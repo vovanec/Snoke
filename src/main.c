5777d125ce5489bad03a2a4c9f20ad07b652d7d2
Vovan Kuznetsov <vkuznet@juniper.net>
2016-01-07 01:17:55 -0800

    Code reformat

--- src/main.c
+++ src/main.c
@@ -2,156 +2,156 @@
 
 #include "ui.h"
 
-	
+
 // Key values for AppMessage Dictionary
 enum {
-	STATUS_KEY = 0,
-	MESSAGE_TYPE_KEY = 1,
-	MESSAGE_KEY = 2
+    STATUS_KEY = 0,
+    MESSAGE_TYPE_KEY = 1,
+    MESSAGE_KEY = 2
 };
 
 enum {
-  MSG_TYPE_STOCKS = 0,
-  MSG_TYPE_WEATHER = 1
+    MSG_TYPE_STOCKS = 0,
+    MSG_TYPE_WEATHER = 1
 };
 
 enum {
-  STATUS_OK = 0,
-  STATUS_ERROR = 1
+    STATUS_OK = 0,
+    STATUS_ERROR = 1
 };
 
 // Write message to buffer & send
-void query_backend(int message_type){
-  
-	DictionaryIterator *iter;
-	
-	app_message_outbox_begin(&iter);
-	dict_write_uint8(iter, STATUS_KEY, STATUS_OK);
-	dict_write_uint8(iter, MESSAGE_TYPE_KEY, message_type);
-	dict_write_end(iter);
-  
-  app_message_outbox_send();
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Message has been sent\n");
+void query_backend(int message_type) {
+
+    DictionaryIterator *iter;
+
+    app_message_outbox_begin(&iter);
+    dict_write_uint8(iter, STATUS_KEY, STATUS_OK);
+    dict_write_uint8(iter, MESSAGE_TYPE_KEY, message_type);
+    dict_write_end(iter);
+
+    app_message_outbox_send();
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Message has been sent\n");
 }
 
 // Called when a message is received from PebbleKitJS
 static void in_received_handler(DictionaryIterator *received, void *context) {
-  
-	Tuple *tuple;
-  int status;
-  int message_type;
-  char* message;
-	
-	tuple = dict_find(received, STATUS_KEY);
-	if(!tuple) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Could not extract status code from backend response.\n");
-    return;
-  }
-  status = (int)tuple->value->uint32;
-  
-	tuple = dict_find(received, MESSAGE_TYPE_KEY);
-	if(!tuple) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Could not extract message type from backend response.\n");
-    return;
-  }
-  message_type = (int)tuple->value->uint32;
 
-	tuple = dict_find(received, MESSAGE_KEY);
-	if(!tuple) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Could not extract message from backend response.\n");
-    return;
-  }
-  message = tuple->value->cstring;    
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Received message type %d, message string: %s.\n", message_type, message);
-  
-  if (status != STATUS_OK) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Backend responded with status %d. Error message: %s.\n", status, message);
-    return;
-  }
-  
-  switch(message_type) {
+    Tuple *tuple;
+    int status;
+    int message_type;
+    char* message;
+
+    tuple = dict_find(received, STATUS_KEY);
+    if(!tuple) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Could not extract status code from backend response.\n");
+        return;
+    }
+    status = (int)tuple->value->uint32;
+
+    tuple = dict_find(received, MESSAGE_TYPE_KEY);
+    if(!tuple) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Could not extract message type from backend response.\n");
+        return;
+    }
+    message_type = (int)tuple->value->uint32;
+
+    tuple = dict_find(received, MESSAGE_KEY);
+    if(!tuple) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Could not extract message from backend response.\n");
+        return;
+    }
+    message = tuple->value->cstring;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Received message type %d, message string: %s.\n", message_type, message);
+
+    if (status != STATUS_OK) {
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Backend responded with status %d. Error message: %s.\n", status, message);
+        return;
+    }
+
+    switch(message_type) {
     case MSG_TYPE_STOCKS:
-      set_stock_price(message);    
-      break;
+        set_stock_price(message);
+        break;
     case MSG_TYPE_WEATHER:
-      set_weather_info(message);
-      break;
+        set_weather_info(message);
+        break;
     default:
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Received unknown message type %d.\n", message_type);
-  }
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Received unknown message type %d.\n", message_type);
+    }
 }
 
 // Called when an incoming message from PebbleKitJS is dropped
-static void in_dropped_handler(AppMessageResult reason, void *context) {	
-  
-  APP_LOG(APP_LOG_LEVEL_ERROR, "App message dropped.\n");
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+
+    APP_LOG(APP_LOG_LEVEL_ERROR, "App message dropped.\n");
 }
 
 // Called when PebbleKitJS does not acknowledge receipt of a message
 static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-  
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Backend could not accept app message.\n");
+
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Backend could not accept app message.\n");
 }
 
 
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
-  
-  //APP_LOG(APP_LOG_LEVEL_DEBUG, "Tick received: %d:%d\n", tick_time->tm_hour, tick_time->tm_min);
-  
-  if (tick_time->tm_min == 0) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Querying backend for stock info update.\n");
-    query_backend(MSG_TYPE_STOCKS);
-  } 
-  
-  if (tick_time->tm_min % 20 == 0) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Querying backend for weather info update.\n");
-    query_backend(MSG_TYPE_WEATHER);
-  }
-  
-  set_time(tick_time);
+
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, "Tick received: %d:%d\n", tick_time->tm_hour, tick_time->tm_min);
+
+    if (tick_time->tm_min == 0) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Querying backend for stock info update.\n");
+        query_backend(MSG_TYPE_STOCKS);
+    }
+
+    if (tick_time->tm_min % 20 == 0) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Querying backend for weather info update.\n");
+        query_backend(MSG_TYPE_WEATHER);
+    }
+
+    set_time(tick_time);
 }
 
 static void update_battery(BatteryChargeState charge_state) {
-  
-  set_battery_percent(charge_state.charge_percent);
+
+    set_battery_percent(charge_state.charge_percent);
 }
 
 static void bluetooth_connection_callback(bool connected) {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Bluetooth is %s.\n", connected ? "connected" : "disconnected");
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Bluetooth is %s.\n", connected ? "connected" : "disconnected");
 }
 
-void init(void) {  
-  
-	show_ui();
-  
-	app_message_register_inbox_received(in_received_handler); 
-	app_message_register_inbox_dropped(in_dropped_handler); 
-	app_message_register_outbox_failed(out_failed_handler);
-		
-	app_message_open(app_message_inbox_size_maximum(),
-                   app_message_outbox_size_maximum());
-  
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
-  
-  update_battery(battery_state_service_peek());
-  battery_state_service_subscribe(&update_battery);  
-    
-  bluetooth_connection_service_subscribe(bluetooth_connection_callback);
+void init(void) {
+
+    show_ui();
+
+    app_message_register_inbox_received(in_received_handler);
+    app_message_register_inbox_dropped(in_dropped_handler);
+    app_message_register_outbox_failed(out_failed_handler);
+
+    app_message_open(app_message_inbox_size_maximum(),
+                     app_message_outbox_size_maximum());
+
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+
+    update_battery(battery_state_service_peek());
+    battery_state_service_subscribe(&update_battery);
+
+    bluetooth_connection_service_subscribe(bluetooth_connection_callback);
 }
 
 void deinit(void) {
-    
-	app_message_deregister_callbacks();
-  tick_timer_service_unsubscribe();
-  battery_state_service_unsubscribe();
-  bluetooth_connection_service_unsubscribe();
-  
-  hide_ui();
+
+    app_message_deregister_callbacks();
+    tick_timer_service_unsubscribe();
+    battery_state_service_unsubscribe();
+    bluetooth_connection_service_unsubscribe();
+
+    hide_ui();
 }
 
 int main( void ) {
-	init();
-	app_event_loop();
-	deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
--- src/ui.c
+++ src/ui.c
@@ -34,137 +34,137 @@ static TextLayer *s_weather;
 
 
 static void handle_window_load(Window *window) {
-  
-  window_set_background_color(s_window, GColorBlack);
-  
-  #ifndef PBL_SDK_3
+
+    window_set_background_color(s_window, GColorBlack);
+
+#ifndef PBL_SDK_3
     window_set_fullscreen(s_window, true);
-  #endif
-  
-  s_res_gothic_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
-  s_res_gothic_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18);  
-  s_res_gothic_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24);
-  s_res_gothic_24 = fonts_get_system_font(FONT_KEY_GOTHIC_28);  
-  
-  // s_bitmaplayer
-  s_bitmaplayer = bitmap_layer_create(GRect(0, 0, 113, 127));
-  s_snoke_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SNOKE_BITMAP_SMALL);
-	
-  //bitmap_layer_set_compositing_mode(s_bitmaplayer, GCompOpAssign);
-  //bitmap_layer_set_alignment(s_bitmaplayer, GAlignCenter);
-  bitmap_layer_set_bitmap(s_bitmaplayer, s_snoke_bitmap);    
-  layer_add_child(window_get_root_layer(s_window), (Layer*)s_bitmaplayer);
-  
-  // s_stocks_label
-  s_stocks_label = text_layer_create(GRect(72, 148, 32, 20));
-  text_layer_set_background_color(s_stocks_label, GColorClear);
-  text_layer_set_text_color(s_stocks_label, GColorWhite);
-  text_layer_set_text(s_stocks_label, "JNPR: ");
-  text_layer_set_font(s_stocks_label, s_res_gothic_18);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_stocks_label);
-  
-  // s_stocks
-  s_stocks = text_layer_create(GRect(105, 148, 38, 20));
-  text_layer_set_background_color(s_stocks, GColorClear);
-  text_layer_set_text_color(s_stocks, GColorWhite);
-  text_layer_set_text(s_stocks, "    ");
-  text_layer_set_text_alignment(s_stocks, GTextAlignmentRight);
-  text_layer_set_font(s_stocks, s_res_gothic_18);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_stocks);
-  
-  // s_time
-  s_time = text_layer_create(GRect(4, 138, 54, 32));
-  text_layer_set_background_color(s_time, GColorClear);
-  text_layer_set_text_color(s_time, GColorWhite);
-  text_layer_set_text(s_time, "00:00");
-  text_layer_set_font(s_time, s_res_gothic_24);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_time);
-  
-  // s_battery_icon
-  s_battery_icon = bitmap_layer_create(GRect(107, 5, 6, 12));
-  bitmap_layer_set_background_color(s_battery_icon, GColorWhite);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_battery_icon);
-  
-  // s_battery_percent
-  s_battery_percent = text_layer_create(GRect(115, 2, 28, 14));
-  text_layer_set_background_color(s_battery_percent, GColorClear);
-  text_layer_set_text_color(s_battery_percent, GColorWhite);
-  text_layer_set_text(s_battery_percent, "100%");
-  text_layer_set_text_alignment(s_battery_percent, GTextAlignmentCenter);
-  text_layer_set_font(s_battery_percent, s_res_gothic_14);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_battery_percent);
-  
-  // s_weather
-  s_weather = text_layer_create(GRect(7, 128, 132, 16));
-  text_layer_set_background_color(s_weather, GColorClear);
-  text_layer_set_text_color(s_weather, GColorWhite);
-  text_layer_set_text(s_weather, "     ");
-  text_layer_set_text_alignment(s_weather, GTextAlignmentCenter);
-  text_layer_set_font(s_weather, s_res_gothic_14);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)s_weather);
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "UI Initialized.\n");  
+#endif
+
+    s_res_gothic_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+    s_res_gothic_18 = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+    s_res_gothic_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24);
+    s_res_gothic_24 = fonts_get_system_font(FONT_KEY_GOTHIC_28);
+
+    // s_bitmaplayer
+    s_bitmaplayer = bitmap_layer_create(GRect(0, 0, 113, 127));
+    s_snoke_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SNOKE_BITMAP_SMALL);
+
+    //bitmap_layer_set_compositing_mode(s_bitmaplayer, GCompOpAssign);
+    //bitmap_layer_set_alignment(s_bitmaplayer, GAlignCenter);
+    bitmap_layer_set_bitmap(s_bitmaplayer, s_snoke_bitmap);
+    layer_add_child(window_get_root_layer(s_window), (Layer*)s_bitmaplayer);
+
+    // s_stocks_label
+    s_stocks_label = text_layer_create(GRect(72, 148, 32, 20));
+    text_layer_set_background_color(s_stocks_label, GColorClear);
+    text_layer_set_text_color(s_stocks_label, GColorWhite);
+    text_layer_set_text(s_stocks_label, "JNPR: ");
+    text_layer_set_font(s_stocks_label, s_res_gothic_18);
+    layer_add_child(window_get_root_layer(s_window), (Layer *)s_stocks_label);
+
+    // s_stocks
+    s_stocks = text_layer_create(GRect(105, 148, 38, 20));
+    text_layer_set_background_color(s_stocks, GColorClear);
+    text_layer_set_text_color(s_stocks, GColorWhite);
+    text_layer_set_text(s_stocks, "    ");
+    text_layer_set_text_alignment(s_stocks, GTextAlignmentRight);
+    text_layer_set_font(s_stocks, s_res_gothic_18);
+    layer_add_child(window_get_root_layer(s_window), (Layer *)s_stocks);
+
+    // s_time
+    s_time = text_layer_create(GRect(4, 138, 54, 32));
+    text_layer_set_background_color(s_time, GColorClear);
+    text_layer_set_text_color(s_time, GColorWhite);
+    text_layer_set_text(s_time, "00:00");
+    text_layer_set_font(s_time, s_res_gothic_24);
+    layer_add_child(window_get_root_layer(s_window), (Layer *)s_time);
+
+    // s_battery_icon
+    s_battery_icon = bitmap_layer_create(GRect(107, 5, 6, 12));
+    bitmap_layer_set_background_color(s_battery_icon, GColorWhite);
+    layer_add_child(window_get_root_layer(s_window), (Layer *)s_battery_icon);
+
+    // s_battery_percent
+    s_battery_percent = text_layer_create(GRect(115, 2, 28, 14));
+    text_layer_set_background_color(s_battery_percent, GColorClear);
+    text_layer_set_text_color(s_battery_percent, GColorWhite);
+    text_layer_set_text(s_battery_percent, "100%");
+    text_layer_set_text_alignment(s_battery_percent, GTextAlignmentCenter);
+    text_layer_set_font(s_battery_percent, s_res_gothic_14);
+    layer_add_child(window_get_root_layer(s_window), (Layer *)s_battery_percent);
+
+    // s_weather
+    s_weather = text_layer_create(GRect(7, 128, 132, 16));
+    text_layer_set_background_color(s_weather, GColorClear);
+    text_layer_set_text_color(s_weather, GColorWhite);
+    text_layer_set_text(s_weather, "     ");
+    text_layer_set_text_alignment(s_weather, GTextAlignmentCenter);
+    text_layer_set_font(s_weather, s_res_gothic_14);
+    layer_add_child(window_get_root_layer(s_window), (Layer *)s_weather);
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "UI Initialized.\n");
 }
 
 static void handle_window_unload(Window* window) {
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Destroying UI.\n");
-  
-  window_destroy(s_window);
-  bitmap_layer_destroy(s_bitmaplayer);
-  text_layer_destroy(s_stocks_label);
-  text_layer_destroy(s_stocks);
-  text_layer_destroy(s_time);
-  bitmap_layer_destroy(s_battery_icon);
-  text_layer_destroy(s_battery_percent);
-  text_layer_destroy(s_weather);
-  gbitmap_destroy(s_snoke_bitmap);
-  //gbitmap_destroy(s_battery_icon_bitmap);  
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Destroying UI.\n");
+
+    window_destroy(s_window);
+    bitmap_layer_destroy(s_bitmaplayer);
+    text_layer_destroy(s_stocks_label);
+    text_layer_destroy(s_stocks);
+    text_layer_destroy(s_time);
+    bitmap_layer_destroy(s_battery_icon);
+    text_layer_destroy(s_battery_percent);
+    text_layer_destroy(s_weather);
+    gbitmap_destroy(s_snoke_bitmap);
+    //gbitmap_destroy(s_battery_icon_bitmap);
 }
 
 void hide_ui(void) {
-  window_stack_remove(s_window, true);
+    window_stack_remove(s_window, true);
 }
 
 void show_ui(void) {
-  
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Initializing UI.\n");
-  
-  s_window = window_create();
-  
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = handle_window_load,
-    .unload = handle_window_unload,
-  });
-  
-  window_stack_push(s_window, true);
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Initializing UI.\n");
+
+    s_window = window_create();
+
+    window_set_window_handlers(s_window, (WindowHandlers) {
+        .load = handle_window_load,
+        .unload = handle_window_unload,
+    });
+
+    window_stack_push(s_window, true);
 }
 
 void set_stock_price(char* price_string) {
-  
-  strncpy(STOCK_PRICE_STR, price_string, STOCK_PRICE_SIZE);
-  STOCK_PRICE_STR[STOCK_PRICE_SIZE - 1] = '\0';
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting current stock price to %s\n", STOCK_PRICE_STR);  
-  text_layer_set_text(s_stocks, STOCK_PRICE_STR);
+
+    strncpy(STOCK_PRICE_STR, price_string, STOCK_PRICE_SIZE);
+    STOCK_PRICE_STR[STOCK_PRICE_SIZE - 1] = '\0';
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting current stock price to %s\n", STOCK_PRICE_STR);
+    text_layer_set_text(s_stocks, STOCK_PRICE_STR);
 }
 
 void set_weather_info(char* weather) {
-  
-  strncpy(WEATHER_STR, weather, WEATHER_SIZE);
-  WEATHER_STR[WEATHER_SIZE - 1] = '\0';
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting current weather info to %s\n", WEATHER_STR);    
-  text_layer_set_text(s_weather, WEATHER_STR);
+
+    strncpy(WEATHER_STR, weather, WEATHER_SIZE);
+    WEATHER_STR[WEATHER_SIZE - 1] = '\0';
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting current weather info to %s\n", WEATHER_STR);
+    text_layer_set_text(s_weather, WEATHER_STR);
 }
 
 void set_time(struct tm *tick_time) {
-  
-  strftime(TIME_STR, TIME_STR_SIZE, "%H:%M", tick_time);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting current time to %s\n", TIME_STR);
-  text_layer_set_text(s_time, TIME_STR);
+
+    strftime(TIME_STR, TIME_STR_SIZE, "%H:%M", tick_time);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting current time to %s\n", TIME_STR);
+    text_layer_set_text(s_time, TIME_STR);
 }
 
 void set_battery_percent(int battery_percent) {
-  snprintf(BATTERY_PERCENT_STR, BATTERY_PERCENT_SIZE, "%d%%", battery_percent);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting battery percent to %s\n", BATTERY_PERCENT_STR);
-  text_layer_set_text(s_battery_percent, BATTERY_PERCENT_STR);
+    snprintf(BATTERY_PERCENT_STR, BATTERY_PERCENT_SIZE, "%d%%", battery_percent);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Setting battery percent to %s\n", BATTERY_PERCENT_STR);
+    text_layer_set_text(s_battery_percent, BATTERY_PERCENT_STR);
 }
